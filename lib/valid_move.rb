bb84f3e081ef1f5c03d3a59f1dcaae146ff34509
mirthful-flag-5763 <irinagamb@gmail.com>
2019-03-21 23:01:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if board[index]
-
+  if 0 <=x && x <=8
+end 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   if board[index] == 'X' || board[index] === 'O'
