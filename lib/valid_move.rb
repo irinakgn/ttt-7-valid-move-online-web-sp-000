dc55d2630e00141f19cd56f0ed0f1928052bbbc9
mirthful-flag-5763 <irinagamb@gmail.com>
2019-03-21 23:05:27 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-   if  0<=index && index <=8 && !position_taken(board, index)
+   if  0 <=index && index <=8 && !position_taken?(board, index)
      return true
    end
 
